764dfefd568d0538b532bc4bf565d3b3bbcd16be
axxx007xxxz <mattiabrsrn7@gmail.com>
2018-02-05 19:56:03 +0100

    msm8916-common: configs: Move chmod command to ROM's source
    This reverts commit 62815e2d62ae85883ce03fc241a1db931724af96.

--- configs/extract_firmware.sh
+++ configs/extract_firmware.sh
@@ -4,5 +4,4 @@ for file in /firmware/image/*.gz; do
   OUT_FILE=$(basename $file .gz)
   gzip -dc $file > /system/etc/firmware/$OUT_FILE
   chmod 644 /system/etc/firmware/$OUT_FILE
-  chmod 644 /system/build.prop
 done
